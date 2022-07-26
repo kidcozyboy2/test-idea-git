3094dfa819aac21a46a6c5e0392b98748af3099d
kidcozyboy <ken@gmail.com>
2022-07-26 15:00:27 +0800

    bug fix2

--- src/com/duoke/Test.java
+++ src/com/duoke/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello java ");
         System.out.println("hello go ");
-        System.out.println("bug  fixed");
+        System.out.println("bug  fixed,我再次修改了");
+
     }
 }
