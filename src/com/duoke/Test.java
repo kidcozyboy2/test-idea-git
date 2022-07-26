82fddc5cfcb3770c18b0103cd78121cbb081d3bc
kidcozyboy <ken@gmail.com>
2022-07-26 15:03:30 +0800

    master commit

--- src/com/duoke/Test.java
+++ src/com/duoke/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello java ");
         System.out.println("hello go ");
-        System.out.println("bug  fixed");
+        System.out.println("master  fixed");
     }
 }
